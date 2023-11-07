ce1c924e7df567f2593475008c0ef7ee287afdce
AlexVielca <alex.v@vielca.com>
2023-11-07 11:38:38 +0100

    section contacta conmigo

--- script.js
+++ script.js
@@ -37,3 +37,36 @@ document.addEventListener("DOMContentLoaded", function () {
         });
     });
 });
+
+document.addEventListener("DOMContentLoaded", function () {
+    var scrollToTopBtn = document.getElementById("scrollToTopBtn");
+
+    window.onscroll = function () {
+        scrollFunction();
+    };
+
+    function scrollFunction() {
+        if (window.innerWidth > 600) { 
+            if (document.body.scrollTop > 20 || document.documentElement.scrollTop > 20) {
+                scrollToTopBtn.style.display = "block";
+            } else {
+                scrollToTopBtn.style.display = "none";
+            }
+        } else {
+            scrollToTopBtn.style.display = "none";
+        }
+    }
+
+    scrollToTopBtn.addEventListener("click", function () {
+        smoothScrollToTop();
+    });
+
+    function smoothScrollToTop() {
+        var currentPosition = document.documentElement.scrollTop || document.body.scrollTop;
+
+        if (currentPosition > 0) {
+            window.requestAnimationFrame(smoothScrollToTop);
+            window.scrollTo(0, currentPosition - currentPosition / 8);
+        }
+    }
+});
